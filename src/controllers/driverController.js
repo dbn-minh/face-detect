b09f4f4e3078d9f69de45585f38e213aba23274b
dbn-minh <ititiu21243@student.hcmiu.edu.vn>
2024-08-17 13:45:37 +0700

    Driver: completed but need more logic

--- src/controllers/driverController.js
+++ src/controllers/driverController.js
@@ -1,114 +1,54 @@
 import {responseData} from "../config/response.js";
-import {Sequelize} from "sequelize";
-import sequelize from "../config/database.js";
-import initModels from "../models/init-models.js";
-
-let model = initModels(sequelize);
+import * as service from "../services/driverServices.js";
 
 export default class DriverController {
+    // Fetch details students of driver in journey
     static async getDriverDetails(req, res) {
-        // Function to get driver details
         try {
-            let {driver_id} = req.params;
+            const { driver_id } = req.params;
+            const { error, data } = await service.getDriverDetailsById(driver_id);
 
-            let data = await model.Driver.findOne({
-                where: {driverID: driver_id}
-            });
-
-            if (!data) {
-                responseData(res, "Fail", "Driver not found", 404)
+            if (error) {
+                return responseData(res, "Fail", error, 404);
             }
 
-            responseData(res, "Success", data, 200);
+            return responseData(res, "Success", data, 200);
         } catch (e) {
-            responseData(res, "Error ...", e.message, 500);
+            return responseData(res, "Error", e.message, 500);
         }
     }
 
+    // Fetch the complete profile of a driver
     static async getDriverProfile(req, res) {
-        // Function to get driver profile
-
         try {
-            let {id} = req.params;
+            const { driver_id } = req.params;
+            const { error, data } = await service.getDriverProfileById(driver_id);
 
-            let data = await model.User.findOne({
-                where: {userId: id}
-            });
-            if (!data || data.roleID !== 2) {
-                return responseData(res, "Fail", "Driver not found", 404)
+            if (error) {
+                return responseData(res, "Fail", error, 404);
             }
 
-            responseData(res, "Success", data, 200);
+            return responseData(res, "Success", data, 200);
         } catch (e) {
-            responseData(res, "Error ...", e.message, 500);
+            return responseData(res, "Error", e.message, 500);
         }
     }
 
+    // Update the profile of a driver
     static async updateDriverProfile(req, res) {
-        // Function to update driver profile
-
         try {
-            let {id} = req.params;
-            let {name, phoneNumber, email} = req.body;
+            const { driver_id } = req.params;
+            const updatedDriverData = req.body;
 
-            let data = await model.User.findOne({
-                where: {userId: id}
-            });
-            if (!data || data.roleID !== 2) {
-                responseData(res, "No driver for update", "", 404);
+            const { error, data } = await service.updateDriverProfileById(driver_id, updatedDriverData);
+
+            if (error) {
+                return responseData(res, "Fail", error, 404);
             }
 
-            await data.update({name, phoneNumber, email});
-
-
-            responseData(res, "Success update", data, 200);
+            return responseData(res, "Success", data, 200);
         } catch (e) {
-            responseData(res, "Error ...", e.message, 500);
+            return responseData(res, "Error", e.message, 500);
         }
-    }
-
-    static async createDriverProfile(req, res) {
-        // Function to create driver profile
-        try {
-            let {name, phoneNumber, email} = req.body;
-
-            let data = await model.User.findOne({
-                where: {name, phoneNumber, email},
-            });
-
-            !data
-                ? await model.User.create({name, phoneNumber, email, roleID: 2})
-                : responseData(res, "Create Fail, user is available", data, 404);
-
-            responseData(res, "Success create new driver", data, 200);
-        } catch (e) {
-            responseData(res, "Error ...", e.message, 500);
-        }
-    }
-
-    static async deleteDriverProfile(req, res) {
-        // Function to delete driver profile
-
-        try {
-            let {id} = req.params;
-
-            let data = await model.User.findOne({
-                where: {userId: id}
-            });
-
-            if (!data || data.roleID !== 2) {
-                responseData(res, "Fail", "Driver not found", 404);
-                return;
-            }
-            await data.destroy();
-
-            responseData(res, "Success", "Driver profile deleted successfully", 200);
-        } catch (e) {
-            responseData(res, "Error ...", e.message, 500);
-        }
-    }
-
-    static async logoutDriver(req, res) {
-        // Function to handle driver logout
     }
 }
