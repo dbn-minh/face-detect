d16300e19ba9fcf7280f3ecb60e2dcae8d4edf97
Đoàn Minh <ititiu21243@student.hcmiu.edu.vn>
2024-10-09 22:08:48 +0700

    Teacher: ready

--- src/routes/teacherRoutes.js
+++ src/routes/teacherRoutes.js
@@ -3,10 +3,14 @@ import TeacherController from '../controllers/teacherController.js';
 
 const router = express.Router();
 
-router.get('/details/:teacher_id', TeacherController.getTeacherDetails);
-router.get('/notification/:teacher_id', TeacherController.getNotifications);
-router.get('/tracking/:teacher_id', TeacherController.getBusTracking);
-router.get('/profile/:teacher_id', TeacherController.getTeacherProfile);
-router.put('/profile/:teacher_id', TeacherController.updateTeacherProfile);
+// router.get('/homepage/:teacher_id', TeacherController.getTeacherDetails); // remember to filter the status
+// router.get('/broken-photo/:teacher_id', TeacherController.getNotifications);
+// router.get('/emergency-photo/:teacher_id', TeacherController.getBusTracking); // store in noti, set alight as common
+// router.get('/notification/:teacher_id', TeacherController.getTeacherProfile);
+// router.get('/students/:teacher_id', TeacherController.updateTeacherProfile);
+// router.get('/setting/:teacher_id', TeacherController.updateTeacherProfile);
+// router.put('/setting/:teacher_id', TeacherController.updateTeacherProfile);
+// router.post('/feedback/:teacher_id', TeacherController.updateTeacherProfile);
+//change password
 
 export default router;
